abed4c3115db5f24801026f4566dacb6b8bd75d3
Guido Marucci Blas <guidomb@gmail.com>
2015-04-19 22:35:53 -0300

    Initial commit

---
+++ retweet.rb
@@ -0,0 +1,33 @@
+require 'dotenv'
+require 'tweetstream'
+
+Dotenv.load
+
+TweetStream.configure do |config|
+  config.consumer_key       = ENV['TWITTER_CONSUMER_KEY']
+  config.consumer_secret    = ENV['TWITTER_CONSUMER_SECRET']
+  config.oauth_token        = ENV['ACCOUNT_OAUTH_TOKEN']
+  config.oauth_token_secret = ENV['ACCOUNT_OAUTH_SECRET']
+  config.auth_method        = :oauth
+end
+
+rest_client = Twitter::REST::Client.new do |config|
+  config.consumer_key       = ENV['TWITTER_CONSUMER_KEY']
+  config.consumer_secret    = ENV['TWITTER_CONSUMER_SECRET']
+  config.access_token        = ENV['ACCOUNT_OAUTH_TOKEN']
+  config.access_token_secret = ENV['ACCOUNT_OAUTH_SECRET']
+end
+
+accounts = {
+  "wolox" => 202739320,
+  "woloxiot" => 2892166253,
+  "woloxeng" => 590254332,
+  "syrmoskate" => 1265759042,
+}
+
+stream_client = TweetStream::Client.new
+puts "Following accounts: #{accounts.keys.join(", ")}"
+stream_client.follow(accounts.values) do |status|
+  puts "Retweeting status #{status.text}"
+  rest_client.retweet(status)
+end
